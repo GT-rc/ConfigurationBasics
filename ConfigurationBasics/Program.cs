16f6a2b826776522fdc376fbbc3ea29e548a6d35
Glynnis Thomas <glynnis.thomas@kdol.ks.gov>
2019-06-13 15:44:44 -0500

    pushing practice for wfh

---
+++ ConfigurationBasics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+
+namespace ConfigurationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In Memory Config Collection
+            var settings = new Dictionary<string, string>
+            {
+                ["HelloMessage"] = "Hiya buddy!",
+                ["GoodbyeMessage"] = "So long, buddy!"
+            };
+
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(settings)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables("ConfigurationBasics.");
+            var config = builder.Build();
+
+            Console.WriteLine($"{config["HelloMessage"]}");
+            Console.WriteLine($"{config["GoodbyeMessage"]}");
+
+            Console.ReadKey();
+        }
+    }
+}
